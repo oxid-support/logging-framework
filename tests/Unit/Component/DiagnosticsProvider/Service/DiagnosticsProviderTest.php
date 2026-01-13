ce41d9eb32d6b5f23ef7cd979db5382a35475966
OXIDSimonGassenschmidt <simon.gassenschmidt@oxid-esales.com>
2026-01-13 12:47:46 +0100

    TestCaseExploration

---
+++ tests/Unit/Component/DiagnosticsProvider/Service/DiagnosticsProviderTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidSupport\LoggingFramework\Tests\Unit\Component\DiagnosticsProvider\Service;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+class DiagnosticsProviderTest {
+
+}
